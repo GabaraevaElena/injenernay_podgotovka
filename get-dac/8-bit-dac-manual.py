574e7645261504fe49ffdcc3e7e9bf6f27368da4
GabaraevaElena <huawei731_an@ouylook.com>
2026-05-04 11:42:11 +0300

    good day

--- get-dac/8-bit-dac-manual.py
+++ get-dac/8-bit-dac-manual.py
@@ -1,25 +1,31 @@
 import RPi.GPIO as GPIO
-GPIO.setmode(GPIO.BCM)
-outp=[16,20,21,25,26,17,27,22]
-GPIO.setup(outp,GPIO.OUT)
-dyn_range=float(input())
-def voltage_to_nomber(voltage):
-    if not (0.0<=voltage<=dyn_range):
-        print(f"Напряжение выходит за динамический диапазон ЦАП (0.00- {dyn_range:.2f} B)")
-        print("Устанавливаем 0.0 B")
-        return 0
+import time
 
-    return int(voltage/dyn_range*255)
-def number_to_dac(value):
-    GPIO.output(outp,[int(element) for element in bin(value)[2:].zfill(8)])
+GPIO.setmode(GPIO.BCM)
+leds = [16,20,21,25,26,17,27,22]
+GPIO.setup(leds, GPIO.OUT)
+GPIO.output(leds, 0)
+dynamic_range=3.3
+def voltage_to_number(voltage):
+    if not(0.0<=voltage<=dynamic_range):
+        print('Напряжение за рамками диапазона, сброс до 0.0 В')
+        return 0
+    return int(voltage/dynamic_range*255)
+
+def dec2bin(val):
+    return[int(el) for el in bin(val)[2:].zfill(8)]
+
 try:
     while True:
         try:
-            voltage=float(input("Введите напряжение в вольтах:"))
-            number=voltage_to_nomber(voltage)
-            number_to_dac(number)
+            voltage = float(input('Введите напряжение: '))
+            number = voltage_to_number(voltage)
+            n = dec2bin(number)
+            GPIO.output(leds, n)
+            print(n)
         except ValueError:
-            print("Вы ввели не число.Попробуйте ещё раз\n")
+            print('Вы ввели не число...зачем...')
+            GPIO.output(leds, 0)
 finally:
-    GPIO.output(outp,0)
+    GPIO.output(leds, 0)
     GPIO.cleanup()
--- get-dac/mcp4725_driver.py
+++ get-dac/mcp4725_driver.py
@@ -1,41 +1,46 @@
 import smbus
+
 class MCP4725:
-    def __init__(self,dynamic_range, address=0x61,verbose=True):
-        self.bus=smbus.SMBus(1)
+    def __init__(self, dynamic_range, address=0x61, verbose=True):
+        self.bus = smbus.SMBus(1)
+        self.address = address
+        self.wm = 0x00
+        self.pds = 0x00
+        self.verbose = verbose
+        self.dynamic_range = dynamic_range
 
-        self.address=address
-        self.wm=0x00
-        self.pds=0x00
-
-        self.verbose=verbose
-        self.dynamic_range=dynamic_range
     def deinit(self):
         self.bus.close()
-    def set_number(self,number):
-       if not isinstance(number,int):
-           print("На ЦАП можно подавать только целые числа")
-       if not (0<=number<=4095):
-           print("Число выходит за разрядность MCP4752 (12 бит)")
-       first_byte=self.wm| self.pds| number>>8
-       second_byte= number & 0xFF
-       self.bus.write_byte_data(0x61, first_byte, second_byte)
-       if self.verbose:
-        print(f"Число: {number}, отправленные по I2C данные: [0x{(self.address << 1):02X}, 0x{first_byte:02X}, 0x{second_byte:02X}]\n")
-    def set_voltage(self,voltage):
-        if not (0.0<=voltage<=self.dynamic_range):
-           print(f"Напряжение выходит за динамический диапазон ЦАП (0.00- {self.dynamic_range:.2f} В)")
-           print("Устанавливаем 0.0 В")
-           return 0
-        self.set_number(int(voltage/self.dynamic_range*4095))
-if  __name__=="__main__":
-    try:
-        dac = MCP4725(5.0)
+    def set_number(self, number):
+        if not isinstance(number, int):
+            print("Dai celoye chislo")
+        elif not (0<=number<=4095):
+            print('Out of range (12 bit)')
+        else:
+            first_byte = self.wm|self.pds|number>>8
+            second_byte = number & 0xFF
+            self.bus.write_byte_data(0x61, first_byte, second_byte)
 
+            if self.verbose:
+                print(f'Chislo: {number}, otpr po I2C: [0x{(self.address<<1):02X}, 0x{first_byte:02X}, 0x{second_byte:02X}]\n')
+    
+    def set_voltage(self, voltage):
+        if not(0.0<=voltage<=self.dynamic_range):
+            print('Напряжение за рамками диапазона, сброс до 0.0 В')
+        else:
+            val = int(voltage/self.dynamic_range*4095)
+            self.set_number(val)
+
+if __name__ == "__main__":
+    try:
+        dac = MCP4725(5, 0x61, True)
         while True:
             try:
-                voltage=float(input("Введите напряжение в вольтах:"))
+                voltage = float(input('Введите напряжение: '))
                 dac.set_voltage(voltage)
             except ValueError:
-               print("Вы ввели не число.Попробуйте ещё раз\n")
+                print('Вы ввели не число...зачем...')
+            except KeyboardInterrupt:
+                print('Thank you for using RaspberryPie, bye!')
     finally:
         dac.deinit()
