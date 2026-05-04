013c1688007fe7fc002dc20cd12249b82c8b8628
GabaraevaElena <huawei731_an@outlook.com>
2026-05-04 12:31:39 +0300

    good day

--- get-dac/8-bit-dac-manual.py
+++ get-dac/8-bit-dac-manual.py
@@ -1,31 +1,31 @@
 import RPi.GPIO as GPIO
-import time
+import time 
 
 GPIO.setmode(GPIO.BCM)
-leds = [16,20,21,25,26,17,27,22]
+leds=[16, 20, 21, 25, 26, 17, 27, 22]
 GPIO.setup(leds, GPIO.OUT)
-GPIO.output(leds, 0)
-dynamic_range=3.3
+GPIO.output(leds,0)
+dynamic_range= 3.183
 def voltage_to_number(voltage):
-    if not(0.0<=voltage<=dynamic_range):
-        print('Напряжение за рамками диапазона, сброс до 0.0 В')
+    if not (0.0 <= voltage <= dynamic_range):
+        print("Напряжение выход за динамический диапозон ЦАП, до 0.00")
         return 0
-    return int(voltage/dynamic_range*255)
+    return int(voltage / dynamic_range * 255)
 
-def dec2bin(val):
-    return[int(el) for el in bin(val)[2:].zfill(8)]
-
-try:
+def dac2bin(value):
+    return[int(element) for element in bin(value)[2:].zfill(8)]
+try: 
     while True:
         try:
-            voltage = float(input('Введите напряжение: '))
+            voltage = float(input("Введите напряжение в Вольтах: "))
             number = voltage_to_number(voltage)
-            n = dec2bin(number)
-            GPIO.output(leds, n)
-            print(n)
+            n = dac2bin(number)
+            print (n)
+
         except ValueError:
-            print('Вы ввели не число...зачем...')
-            GPIO.output(leds, 0)
+            print("Вы ввели не число. Попробуйте ещё раз\n")
+            GPIO.output(leds,0)
+
 finally:
     GPIO.output(leds, 0)
     GPIO.cleanup()
